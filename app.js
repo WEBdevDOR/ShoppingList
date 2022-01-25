b2a54e7c2b290e28aba1f1e139b563fbcb295ca6
WEBdevDOR_main_PC <webdev.dor@gmail.com>
2022-01-25 14:56:45 +0000

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+// Variables
+const interface = document.querySelector('.interface');
+const input = document.querySelector('input');
+const addBtn = document.querySelector('#add');
+const clearBtn = document.querySelector('#clear');
+const list = document.querySelector('.list');
+
+// Functions
+const addToList = function() {
+  if (input.value !== '') {
+    const newLi = document.createElement('li');
+    newLi.append(input.value);
+    list.append(newLi);
+    input.value = '';
+  }
+};
+const clearList = function() {
+  list.innerText = '';
+}
+
+// Events
+addBtn.addEventListener('click', addToList);
+input.addEventListener('keydown', function(e) {
+  if (e.key === 'Enter') {
+    addToList();
+  }
+})
+clearBtn.addEventListener('click', clearList);
